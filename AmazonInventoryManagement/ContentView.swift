4f9be0fb0f2ff9dd6c37207f1b32f70bd1d32095
LezardWhiteGlint <30715385+LezardWhiteGlint@users.noreply.github.com>
2020-04-05 13:35:56 +0800

    Initial Commit

---
+++ AmazonInventoryManagement/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  AmazonInventoryManagement
+//
+//  Created by Lezardvaleth on 2020/4/5.
+//  Copyright © 2020 Lezardvaleth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
